d8c300b54b19678dee395a7f5669dc299ccdcf3a
ignacio429 <ignacioleytonvilla4@gmail.com>
2026-04-24 16:45:33 -0400

    MAS TAREAS

---
+++ a.py
@@ -0,0 +1,18 @@
+candidato1=input("ingrese el nombre de su candidato") 
+candidato2=input ("ingrese el nombre de su segundo candidato")
+v1=0
+v2=0
+num=int(input("ingrese la cantidad de votantes:"))
+
+for i in range(num):
+    voto=int(input("por quien votara. 1.-{candidato1} 2-{candidato2}")))
+    if voto==1
+    v1+=1
+    elifvoto=2:
+    v2+=1
+else:
+    print("voto no valido")
+    if v1>v2:
+        print(f"el ganador es {candidato1} con {v1}votos")
+        
+
